f594b59d2fb960bf841b82879b17e104f17c9b19
hoonion90 <clearch21@gmail.com>
2021-08-17 00:29:52 +0900

    Publishing Page

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 const express = require('express');
 const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
 app.use(express.static('.'));
+app.use(favicon(path.join(__dirname, 'img', 'favicon.ico')))
 
 app.get('/', function (request, response) {
     var _url = request.url;
@@ -22,4 +24,22 @@ app.get('/', function (request, response) {
         response.writeHeadg(404);
         response.end('Not found');
     }
-}); app.listen(3000);
+});
+app.get('/test', function (request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    console.log(_url);
+    console.log(queryData);
+    console.log(pathname);
+    if(pathname === '/test'){
+        fs.readFile('./public/find.html', 'utf8', function(err, HTML){
+            response.writeHead(200);
+            response.end(HTML);
+        })
+    } else {
+        response.writeHeadg(404);
+        response.end('Not found');
+    }
+});
+ app.listen(3000);
