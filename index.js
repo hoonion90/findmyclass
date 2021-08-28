07c7ec01687ad1d3433b60ba1488b2ae97d7b797
hoonion90 <clearch21@gmail.com>
2021-08-29 00:13:43 +0900

    Modify Port

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ function numberWithCommas(x) {
 }
 
 // app.listen(3000);
-// const PORT = process.env.PORT;
-// app.listen(PORT);
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-);
+const PORT = process.env.PORT;
+app.listen(PORT);
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
