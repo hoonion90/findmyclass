885da4da4ec31a5d8a0693cc49e2fe8471c8182e
hoonion90 <clearch21@gmail.com>
2021-08-28 19:03:12 +0900

    Modify index, result, lowdb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { LowSync, JSONFileSync } from "lowdb";
+import bodyParser from 'body-parser';
 import _ from 'lodash';
 import url from 'url';
 import express from 'express';
@@ -14,9 +15,10 @@ db.chain = _.chain(db.data);
 app.set('view engine','ejs');
 app.set('views',__dirname);
 app.use(express.static('.'));
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', function (request, response) {
-    let visitors = db.chain.get('visitors').value();
     let players = db.chain.get('players').value();
     let hashClass = db.chain.get('class_count').value();
     let max = Object.keys(hashClass).reduce((a, v) => Math.max(a, hashClass[v]), -Infinity);
@@ -25,7 +27,6 @@ app.get('/', function (request, response) {
     let maxClassName = _.get(db.chain.get('class_name').value(),maxClass[0]);
     db.chain.update('visitors', n => n + 1).value();
     db.write();
-    console.log(visitors);
     response.render('./public/index.ejs',{'players': numberWithCommas(players), mostClass: maxClassName, mostClassImg: maxClassImg});
 });
 app.get('/test', function (request, response) {
@@ -39,12 +40,19 @@ app.get('/result', function (request, response) {
     let result = _.get(resultObj, resultClass);
     let className = _.get(db.chain.get('class_name').value(), resultClass);
     let classImg = _.get(db.chain.get('class_img').value(), resultClass);
-    response.render('./public/result_page.ejs',{className: className, classImg: classImg ,resultmsg: result});
+    let class_count_sum = db.chain.get('class_count_sum').value();
+    let class_count = _.get(db.chain.get('class_count').value(), queryData.c);
+    let poc = Math.round(class_count / class_count_sum * 100000)/1000;
+    
+    response.render('./public/result_page.ejs',{className: className, classImg: classImg ,resultmsg: result, poc: poc});
 });
 
-app.post('/result', function (request, response) {
-    let _url = request.url;
-    let queryData = url.parse(_url, true).query;
+app.post('/resultcount', function (request, response) {
+    let u_class = request.body.class;
+    db.chain.get('class_count').update(u_class, n=> n + 1 ).value();
+    db.write();
+    db.chain.update('class_count_sum', n => n + 1).value();
+    db.write();
     response.writeHead(200);
     response.end();
 });
